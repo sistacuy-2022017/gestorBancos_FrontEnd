cee8247e55f572b47b7bedcb8aa44b6928cd7607
dalvarez-2022277 <dalvarez-2022277@kinal.edu.gt>
2024-05-31 11:38:42 -0600

    se agrega input con estilo e imagen de ejemplo

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -18,32 +18,54 @@ export const Input = ({
   };
 
   return (
-    <>
-      <div className="label">
-        <span className="">{label}</span>
+    <div className="mb-8">
+      <div className="flex items-center border border-gray-300 py-2 px-3 rounded-2xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          {field === "email" ? (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+            />
+          ) : (
+            <path
+              fillRule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clipRule="evenodd"
+            />
+          )}
+        </svg>
+        {textarea ? (
+          <textarea
+            className="pl-2 w-full outline-none border-none"
+            type={type}
+            value={value}
+            onChange={handleValueChange}
+            onBlur={handleInputBlur}
+            rows={5}
+            style={{ maxWidth: "400px" }}
+          />
+        ) : (
+          <input
+            className="pl-2 w-full outline-none border-none"
+            type={type}
+            value={value}
+            onChange={handleValueChange}
+            onBlur={handleInputBlur}
+            placeholder={label}
+          />
+        )}
       </div>
-      {textarea ? (
-        <textarea
-          type={type}
-          value={value}
-          onChange={handleValueChange}
-          onBlur={handleInputBlur}
-          rows={5}
-          style={{ maxWidth: "400px" }}
-        />
-      ) : (
-        <input
-          className="input"
-          type={type}
-          value={value}
-          onChange={handleValueChange}
-          onBlur={handleInputBlur}
-        />
-      )}
-
-      <span className="error-message">
+      <span className="error-message text-red-500 text-sm mt-1">
         {showErrorMessage && validationMessage}
       </span>
-    </>
+    </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
-import { Input } from "./Input.jsx";
+import { Input } from "./Input.jsx"; // Asegúrate de que la ruta sea correcta
 import "./Login.css";
 
 export const Login = ({ switchAuthHandler }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChange = (value, field) => {
+    if (field === "email") {
+      setEmail(value);
+    } else if (field === "password") {
+      setPassword(value);
+    }
+  };
+
   return (
     <>
       <div className="form-container">
@@ -37,50 +48,22 @@ export const Login = ({ switchAuthHandler }) => {
                 <p className="text-sm font-normal text-gray-600 mb-8 text-center lg:text-left">
                   Welcome Back
                 </p>
-                <div className="flex items-center border border-gray-300 mb-8 py-2 px-3 rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                    />
-                  </svg>
-                  <input
-                    id="email"
-                    className="pl-2 w-full outline-none border-none"
-                    type="email"
-                    name="email"
-                    placeholder="Email Address"
-                  />
-                </div>
-                <div className="flex items-center border border-gray-300 mb-12 py-2 px-3 rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input
-                    className="pl-2 w-full outline-none border-none"
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                  />
-                </div>
+                <Input
+                  field="email"
+                  label="Email Address"
+                  value={email}
+                  onChangeHandler={handleInputChange}
+                  type="email"
+                  showErrorMessage={false}
+                />
+                <Input
+                  field="password"
+                  label="Password"
+                  value={password}
+                  onChangeHandler={handleInputChange}
+                  type="password"
+                  showErrorMessage={false}
+                />
                 <button
                   type="submit"
                   className="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
