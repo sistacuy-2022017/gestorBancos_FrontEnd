e840086d422c2b519b3470ab0d000ae209f4efe3
dalvarez-2022277 <dalvarez-2022277@kinal.edu.gt>
2024-05-31 10:43:51 -0600

    se agrega login arreglando bugs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,53 +1,51 @@
 import { useState } from "react";
 import { Input } from "./Input.jsx";
-//import { Logo } from "./Logo";
-//import { useLogin } from "../shared/hooks";
 import "./Login.css";
 
 export const Login = ({ switchAuthHandler }) => {
   return (
     <>
       <div className="form-container">
-        <div class="h-screen flex">
+        <div className="h-screen flex w-full max-w-screen-lg mx-auto bg-white rounded-md shadow-2xl">
           <div
-            class="hidden lg:flex w-full lg:w-1/2 login_img_section
+            className="hidden lg:flex w-1/2 login_img_section
           justify-around items-center"
           >
             <div
-              class=" 
+              className=" 
                   bg-black 
                   opacity-20 
                   inset-0 
                   z-0"
             ></div>
-            <div class="w-full mx-auto px-20 flex-col items-center space-y-6">
-              <h1 class="text-white font-bold text-4xl font-sans">
+            <div className="w-full mx-auto px-20 flex-col items-center space-y-6">
+              <h1 className="text-white font-bold text-4xl font-sans">
                 Simple App
               </h1>
-              <p class="text-white mt-1">The simplest app to use</p>
-              <div class="flex justify-center lg:justify-start mt-6">
+              <p className="text-white mt-1">The simplest app to use</p>
+              <div className="flex justify-center lg:justify-start mt-6">
                 <a
                   href="#"
-                  class="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2"
+                  className="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2"
                 >
                   Get Started
                 </a>
               </div>
             </div>
           </div>
-          <div class="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
-            <div class="w-full px-8 md:px-32 lg:px-24">
-              <form class="bg-white  rounded-md shadow-2xl p-5">
-                <h1 class="text-gray-800 font-bold text-2xl mb-1">
+          <div className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
+            <div className="w-full px-8 md:px-16 lg:px-12">
+              <form className="bg-white rounded-md shadow-2xl p-5">
+                <h1 className="text-gray-800 font-bold text-2xl mb-1">
                   Hello Again!
                 </h1>
-                <p class="text-sm font-normal text-gray-600 mb-8">
+                <p className="text-sm font-normal text-gray-600 mb-8">
                   Welcome Back
                 </p>
-                <div class="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
+                <div className="flex items-center border border-gray-300 mb-8 py-2 px-3 rounded-2xl">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-gray-400"
+                    className="h-5 w-5 text-gray-400"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -61,16 +59,16 @@ export const Login = ({ switchAuthHandler }) => {
                   </svg>
                   <input
                     id="email"
-                    class=" pl-2 w-full outline-none border-none"
+                    className="pl-2 w-full outline-none border-none"
                     type="email"
                     name="email"
                     placeholder="Email Address"
                   />
                 </div>
-                <div class="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
+                <div className="flex items-center border border-gray-300 mb-12 py-2 px-3 rounded-2xl">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-gray-400"
+                    className="h-5 w-5 text-gray-400"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
@@ -81,7 +79,7 @@ export const Login = ({ switchAuthHandler }) => {
                     />
                   </svg>
                   <input
-                    class="pl-2 w-full outline-none border-none"
+                    className="pl-2 w-full outline-none border-none"
                     type="password"
                     name="password"
                     id="password"
@@ -90,18 +88,18 @@ export const Login = ({ switchAuthHandler }) => {
                 </div>
                 <button
                   type="submit"
-                  class="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
+                  className="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
                 >
                   Login
                 </button>
-                <div class="flex justify-between mt-4">
-                  <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">
+                <div className="flex justify-between mt-4">
+                  <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">
                     Forgot Password ?
                   </span>
 
                   <a
                     href="#"
-                    class="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all"
+                    className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all"
                   >
                     Don't have an account yet?
                   </a>
