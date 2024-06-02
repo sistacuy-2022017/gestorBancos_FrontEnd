5c5cff6f5052dd0a9ccc8f7acea031dfeeab6705
dbarton-2019164 <you@example.comdbarton-2019164@kinal.edu.gt>
2024-06-01 18:57:02 -0600

    Pagina de servicios y ofertas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { Login } from './components/Login.jsx'
+import Services from './components/Servicios.jsx'
+import Ofertas from './components/Ofertas-services.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
-    <Login/>
+      <Ofertas />
+      {/*<Services/> Para probar el de servicios*/}
+      {/*<Ofertas/> Para probar el de ofertas*/}
     </>
   )
 }
---
+++ src/components/servicesLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Servicios.css';
+
+const Layout = ({ children }) => {
+    return (
+        <React.Fragment>
+            {children} {}
+        </React.Fragment>
+    );
+};
+
+export default Layout;
