874c96bac708deb98d90a070029bda648d323478
dalvarez-2022277 <dalvarez-2022277@kinal.edu.gt>
2024-06-03 08:27:25 -0600

    se agrega componente de navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { Login } from './components/Login.jsx'
+import { Navbar } from './components/navbar/navbar.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
-    <Login/>
+      <Login />
     </>
   )
 }
